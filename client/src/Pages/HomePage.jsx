5b111aeeb217ba46914e007cb611f2cb129e3557
Animesh <2025.cse18.animeshbag@gmail.com>
2023-08-18 00:45:34 +0530

    Home Page created

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,5 +1,20 @@
+import logo from './../Assets/6914347.jpg';
+
 const HomePage = () => {
-    return <h1>HomePage</h1>
+    return (
+        <>
+            <div className='main-container'>
+                <section className="greeting">
+                    <h1>Welcome to <span>Taskify</span></h1>
+                    <p>Manage all your Tasks in efficient way</p>
+                    <button>Explore</button>
+                </section>
+                <section className="img">
+                    <img src={logo} alt="Welcome" />
+                </section>
+            </div>
+        </>
+    )
 }
 
 export default HomePage;
