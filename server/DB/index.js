cff9cfd73ef5065bd2c6bc9a0622d8248ce03e6a
Animesh <2025.cse18.animeshbag@gmail.com>
2024-01-25 02:19:41 +0530

    index and app.js updated and type changed to module

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
--- server/index.js
+++ server/index.js
@@ -1,20 +1,15 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors")
-const connectDB = require("./DB");
+import dotenv from "dotenv";
+import connectDB from "./DB";
+import app from "./app";
 
-const app = express();
-const port = process.env.PORT;
-
-app.use(express.json());
-app.use(cors());
+dotenv.config();
 
 connectDB()
 .then(() => {
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running on port ${port}`);
     })
 })
 .catch((err) => {
-    console.error(err);
+    console.error("MongoDB Connection failed!!!" + err);
 })
